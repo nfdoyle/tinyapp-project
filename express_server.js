b8b10e33004436d2b885aac02a94e3ace9625e96
nfdoyle <nickdoyle98@gmail.com>
2019-01-16 01:28:18 +0000

    added generateRandomString function

--- express_server.js
+++ express_server.js
@@ -8,8 +8,14 @@ allow users to shorten long URLs much like TinyURL.com and bit.ly do.
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+return Math.random().toString(36).replace('0.', '').slice(4);
+}
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -18,6 +24,15 @@ var urlDatabase = {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls.json", (req, res) => {
