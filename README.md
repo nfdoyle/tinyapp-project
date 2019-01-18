4ed1cb766b3902f2b15fcf831b2049a9441c548e
nfdoyle <nickdoyle98@gmail.com>
2019-01-18 08:29:31 +0000

    layout touched up and readme added

---
+++ README.md
@@ -0,0 +1,19 @@
+# TinyApp web link-compressor
+
+## Problem Statement
+
+A web-based app designed to condense URLs into a condensed version for ease of use and sharing.
+
+## Particulars
+
+User account information is stored server side with moderate security already implemented - a password hashing system and AES encrypted cookies. User must be registered in order to create, edit, and save links. Basic ejs template linked from third party.
+
+## Expected Usage
+
+The server should be started from the command line, in the following manner:
+
+`npm start`
+
+### Note: Easter egg in URL index
+
+@nfdoyle -- 18 January 2019
--- express_server.js
+++ express_server.js
@@ -9,7 +9,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+//const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt');
+const cookieSession = require('cookie-session')
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -32,22 +34,31 @@ const new_urlDatabase = {
   }
 }
 
+const defaultUserPasswords = {
+  Steve: bcrypt.hashSync("tony-is-my-best-friend", 10),
+  Tony: bcrypt.hashSync("tony-is-my-best-friend", 10)
+}
+
 const users = { 
   "TheCaptain": {
     id: "TheCaptain", 
     email: "captainamerica@shield.gov", 
-    password: "tony-is-my-best-friend"
+    password: defaultUserPasswords.Steve
   },
  "IronMan": {
     id: "IronMan", 
     email: "stark@stark.com", 
-    password: "tony-is-my-best-friend"
+    password: defaultUserPasswords.Tony
   }
 }
 
-app.use(cookieParser());
+//app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
+app.use(cookieSession({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 
 app.set("view engine", "ejs");
   //tmp = req.body.longURL;
@@ -108,37 +119,33 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   if (req.body.email == '' || req.body.password == ''){
     res.statusCode = 400;
-    //res.end();
+    npmres.redirect("/error");
   }
   let userid;
-  // if (users[req.body.email]){
-  //   console.log("found");
-  // } else {
-  //   res.statusCode = 400;
-  //   //res.end();
-  // }
-  console.log(req.body.email);
+  
   for (user in users){
-    console.log("loop:");
-    console.log(user);
-    console.log(typeof user);
-    console.log(users[user].email);
+
     if (users[user].email == req.body.email){
-      console.log("successful if");
-      console.log(req.body.email);
+
       
       userid = users[user].id;
     }
   }
-  if (users[userid].password !== req.body.password){
-    res.statusCode = 400;
-    res.end();
-  } else {
+  //let hashedPass = bcrypt.hashSync(req.body.password, 10);
+  //if (users[userid].password !== hashedPass) {(
+
+  if (bcrypt.compareSync(req.body.password, users[userid].password)){
     console.log("logged in id: " + userid);
-    res.cookie("user_id", userid);
-    templateVars.user.id = req.cookies["user_id"];
+    //res.cookie("user_id", userid);
+    req.session.user_id = userid;
+    //templateVars.user.id = req.cookies["user_id"];
+    templateVars.user.id = req.session.user_id;
     console.log("TVAR in post reg:", templateVars);
-    res.redirect(`/urls`)   
+    res.redirect(`/urls`) 
+  } else {
+    
+    res.statusCode = 400;
+    res.render('error', templateVars);  
   }     
 });
 
@@ -162,19 +169,22 @@ app.post("/register", (req, res) => {
   users[newid] = {};
   users[newid].id = newid;
   users[newid].email = req.body.email;
-  users[newid].password = req.body.password;
+  users[newid].password = bcrypt.hashSync(req.body.password, 10);
   console.log(users);
 
 
-  res.cookie("user_id", users[newid].id);
-  templateVars.user.id = req.cookies["user_id"];
+  //res.cookie("user_id", users[newid].id);
+  req.session.user_id = users[newid].id;
+  //templateVars.user.id = req.cookies["user_id"];
+  templateVars.user.id = req.session.user_id
   console.log("TVAR in post reg:", templateVars);
   res.redirect(`/urls`)        
 });
 
 app.get("/logout", (req, res) => {
   // do biz
-  res.clearCookie("user_id");
+  //res.clearCookie("user_id");
+  req.session = null;
   templateVars.user = {};
   
   res.redirect(`/urls`)        
@@ -203,9 +213,11 @@ app.post('/urls/:id', (req, res) => {
 app.get("/urls", (req, res) => {
   templateVars.urls = new_urlDatabase;
   console.log(new_urlDatabase);
-  console.log(req.cookies["user_id"]);
-  if (req.cookies["user_id"]){
-    templateVars.user = users[req.cookies["user_id"]];
+  console.log("cookie sess: ", + req.session.user_id);
+  //if (req.cookies["user_id"]){
+  if (req.session.user_id) {
+    //templateVars.user = users[req.cookies["user_id"]];
+    templateVars.user = users[req.session.user_id];
     } else {
       templateVars.user = {};
     }
