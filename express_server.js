60406c2f93db6a8b5784d0df857ae8884f7c838d
nfdoyle <nickdoyle98@gmail.com>
2019-01-18 00:42:35 +0000

    added registration, login/logout w persistence

--- express_server.js
+++ express_server.js
@@ -18,6 +18,19 @@ const urlDatabase = {
 
 const templateVars = {};
 
+const users = { 
+  "TheCaptain": {
+    id: "TheCaptain", 
+    email: "captainamerica@shield.gov", 
+    password: "tony-is-my-best-friend"
+  },
+ "IronMan": {
+    id: "IronMan", 
+    email: "stark@stark.com", 
+    password: "tony-is-my-best-friend"
+  }
+}
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
@@ -44,6 +57,15 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+app.get("/register", (req, res) => {
+  // let longURL = ...
+  console.log(templateVars);
+  if (!templateVars.user) {
+    templateVars.user = {};
+  }
+  res.render("register", templateVars);
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new", templateVars);
 });
@@ -54,20 +76,83 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randString}`)       
 });
 
-app.post("/login", (req, res) => {
+app.get("/login", (req, res) => {
   // do biz
-  console.log("line 59:" + req.body);
-  res.cookie("username", req.body.username);
   
-  templateVars.username = req.cookies["username"];
-  console.log(templateVars);
+  res.render(`login`, templateVars);        
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.email == '' || req.body.password == ''){
+    res.statusCode = 400;
+    //res.end();
+  }
+  let userid;
+  // if (users[req.body.email]){
+  //   console.log("found");
+  // } else {
+  //   res.statusCode = 400;
+  //   //res.end();
+  // }
+  console.log(req.body.email);
+  for (user in users){
+    console.log("loop:");
+    console.log(user);
+    console.log(typeof user);
+    console.log(users[user].email);
+    if (users[user].email == req.body.email){
+      console.log("successful if");
+      console.log(req.body.email);
+      
+      userid = users[user].id;
+    }
+  }
+  if (users[userid].password !== req.body.password){
+    res.statusCode = 400;
+    res.end();
+  } else {
+    console.log("logged in id: " + userid);
+    res.cookie("user_id", userid);
+    templateVars.user.id = req.cookies["user_id"];
+    console.log("TVAR in post reg:", templateVars);
+    res.redirect(`/urls`)   
+  }     
+});
+
+app.post("/register", (req, res) => {
+  // do bizIf the e-mail or password are empty strings, send 
+  //back a response with the 400 status code.
+  //If someone tries to register with an existing user's 
+  //email, send back a response with the 400 status code.
+  if (req.body.email == '' || req.body.password == ''){
+    res.statusCode = 400;
+    res.end();
+  }
+  
+  for (user in users) {
+    if (users[user].email === req.body.email){
+      res.statusCode = 400;
+      res.end();
+      }
+    }
+  let newid = generateRandomString();
+  users[newid] = {};
+  users[newid].id = newid;
+  users[newid].email = req.body.email;
+  users[newid].password = req.body.password;
+  console.log(users);
+
+
+  res.cookie("user_id", users[newid].id);
+  templateVars.user.id = req.cookies["user_id"];
+  console.log("TVAR in post reg:", templateVars);
   res.redirect(`/urls`)        
 });
 
-app.post("/logout", (req, res) => {
+app.get("/logout", (req, res) => {
   // do biz
-  res.clearCookie("username");
-  delete templateVars.username;
+  res.clearCookie("user_id");
+  templateVars.user = {};
   
   res.redirect(`/urls`)        
 });
@@ -90,7 +175,14 @@ app.post('/urls/:id', (req, res) => {
 
 app.get("/urls", (req, res) => {
   templateVars.urls = urlDatabase;
-  templateVars.username = req.cookies["username"];
+  console.log(users);
+  console.log(req.cookies["user_id"]);
+  if (req.cookies["user_id"]){
+    templateVars.user = users[req.cookies["user_id"]];
+    } else {
+      templateVars.user = {};
+    }
+  console.log(templateVars); 
   
   res.render("urls_index", templateVars);
 });
